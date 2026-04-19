df6bc1f1078dfc97a231d17484d2392b34c449f1
gyuwonLee20220611 <104904521+gyuwonLee20220611@users.noreply.github.com>
2026-04-20 00:25:51 +0900

    [fixed] HW nfr 수정

--- Assets/HW/temp_gyuwon/GameManager.cs
+++ Assets/HW/temp_gyuwon/GameManager.cs
@@ -7,21 +7,12 @@ public class GameManager
         // 게임 시작 가능 여부를 확인한다.
         // 가능하면 세션을 시작한다.
         // 방 상태를 플레이 중으로 변경한다.
-
-        if (ruleManager.CanStartGame(room))
-        {
-            currentSession.StartGame();
-            room.isPlaying = true;
-        }
     }
 
     public void EndGame(Room room)
     {
         // 게임 세션을 종료한다.
         // 방 상태를 플레이 종료로 변경한다.
-
-        currentSession.EndGame();
-        room.isPlaying = false;
     }
 
     public void ExitGame(Player player)
--- Assets/HW/temp_gyuwon/RoomManager.cs
+++ Assets/HW/temp_gyuwon/RoomManager.cs
@@ -10,30 +10,12 @@ public class RoomManager
         // 새로운 방 객체를 생성한다.
         // 방 정보를 설정한다.
         // 방 목록에 추가한다.
-
-        Room newRoom = new Room();
-        newRoom.roomId = Guid.NewGuid().ToString();
-        newRoom.roomName = roomName;
-        newRoom.maxPlayers = maxPlayers;
-        newRoom.isPlaying = false;
-
-        roomList.Add(newRoom);
         return newRoom;
     }
 
     public List<Room> SearchRoom(string keyword)
     {
         // 키워드와 일치하는 방을 검색한다.
-        List<Room> result = new List<Room>();
-
-        foreach (Room room in roomList)
-        {
-            if (room.roomName.Contains(keyword))
-            {
-                result.Add(room);
-            }
-        }
-
         return result;
     }
 
@@ -42,14 +24,6 @@ public class RoomManager
         // 방 참가 가능 여부를 확인한다.
         // 가능하면 플레이어를 방에 추가한다.
         // 플레이어의 현재 방 정보를 갱신한다.
-
-        if (room.IsFull())
-        {
-            return false;
-        }
-
-        room.AddPlayer(player);
-        player.JoinRoom(room);
         return true;
     }
 
@@ -57,17 +31,10 @@ public class RoomManager
     {
         // 플레이어를 방에서 제거한다.
         // 플레이어의 현재 방 정보를 초기화한다.
-
-        room.RemovePlayer(player);
-        player.LeaveRoom();
     }
 
     public void RemoveEmptyRoom(Room room)
     {
         // 방이 비어 있으면 목록에서 제거한다.
-        if (room.players.Count == 0)
-        {
-            roomList.Remove(room);
-        }
     }
 }
