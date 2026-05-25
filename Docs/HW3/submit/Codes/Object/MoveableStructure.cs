393fdccfa00ac2dcad1ab87f83857f2ead7011eb
jun <jyoung66130@naver.com>
2026-05-25 17:39:30 +0900

    [chore] 과제 제출용 폴더에 Object 코드 추가

---
+++ Docs/HW3/submit/Codes/Object/MoveableStructure.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using Fusion;
+using UnityEngine;
+
+public class MoveableStructure : NetworkBehaviour, IActivatable
+{
+    public bool IsActive { get; set;}
+    [SerializeField] bool firstState;
+    [SerializeField] bool isDisposable;
+    [SerializeField] int triggerCnt;
+    int curTriggerCnt;
+
+    [SerializeField] float moveTime;
+    float curMoveTime;
+    Transform startPos;
+    Transform endPos;
+    [SerializeField] Transform posA;
+    [SerializeField] Transform posB;
+
+    void Awake()
+    {
+        IsActive = firstState;
+        curTriggerCnt = firstState ? triggerCnt : 0;
+        transform.position = posA.position;
+    }
+
+    public void OnActivate(int n)
+    {
+        if(triggerCnt <= 1)
+        {
+            if(!IsActive)
+            {
+                Activate();
+            }
+            else
+            {
+                Deactivate();
+            }
+            if (isDisposable)
+            {
+                triggerCnt = 100;
+            }
+        }
+        else if(triggerCnt == 100)
+        {
+            return;
+        }
+        else
+        {
+            curTriggerCnt += n;
+            if(curTriggerCnt >= triggerCnt)
+            {
+                Activate();
+                if (isDisposable)
+                {
+                    triggerCnt = 100;
+                }
+            }
+            else
+            {
+                Deactivate();
+            }
+        }
+    }
+
+    public void Activate()
+    {
+        IsActive = true;
+        startPos = transform;
+        endPos = posB;
+        StartCoroutine(Move());
+    }
+
+    public void Deactivate()
+    {
+        IsActive = false;
+        startPos = transform;
+        endPos = posA;
+        StartCoroutine(Move());
+    }
+
+    IEnumerator Move()
+    {
+        curMoveTime = 0;
+
+        while(curMoveTime < moveTime)
+        {
+            curMoveTime += Time.deltaTime;
+
+            transform.position = Vector3.Lerp(startPos.position, endPos.position, curMoveTime / moveTime);
+
+            yield return null;
+        }
+    }
+}
---
+++ Docs/HW3/submit/Codes/Object/PressurePlate.cs
@@ -0,0 +1,88 @@
+using System;
+using UnityEngine;
+using Fusion;
+
+public class PressurePlate : OperatableObject
+{
+    int triggerCnt;
+
+    bool operateState;
+    [Networked, OnChangedRender(nameof(OnStateChanged))]
+    private NetworkBool NetworkedOperateState { get; set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        Init();
+    }
+
+    public override void Spawned()
+    {
+        if (Object.HasStateAuthority)
+        {
+            NetworkedOperateState = operateState;
+        }
+    }
+    
+    void Init()
+    {
+        operateState = false;
+        triggerCnt = 0;
+        int index = operateState ? 1 : 0;
+        mesh.material = meshSet[index];
+    }
+
+    void ApplyOperate(int n)
+    {
+        triggerCnt = n;
+        if(triggerCnt > 0 && !operateState)
+        {
+            operateState = true;
+            NetworkedOperateState = true;
+            connectedObjController.OnActivate(1);
+        }
+        else if(triggerCnt == 0 && operateState)
+        {
+            operateState = false;
+            NetworkedOperateState = false;
+            connectedObjController.OnActivate(-1);
+        }
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    void RPC_Operate(int n) => ApplyOperate(n);
+
+    void TriggerCheck()
+    {
+        int res = 0;
+        var triggered = Physics.OverlapBox(transform.position, new Vector3(0.8f, 0.7f, 0.8f), Quaternion.identity);
+        foreach(var entity in triggered)
+        {
+            if(entity.CompareTag("Player") || entity.name == "GrabbableCube" || entity.name == "HeavyCube")
+            {
+                res++;
+            }
+        }
+        RPC_Operate(res);
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        TriggerCheck();
+    }
+
+    protected override void OnStateChanged()
+    {
+        int index = NetworkedOperateState ? 1 : 0;
+        mesh.material = meshSet[index];
+    }
+
+    protected override void ApplyOperate(NetworkObject operatorObject){}
+    void OnDrawGizmos()  
+    {  
+        Gizmos.matrix = transform.localToWorldMatrix;  
+        Gizmos.color = Color.yellow;  
+        Gizmos.DrawWireCube(Vector3.zero, new Vector3(0.8f, 1f, 0.8f));  
+    }
+}
